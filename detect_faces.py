c13cab76363af196d46c25bfd7897c4cf313d09a
Ankita Singh <44571279+Anki246@users.noreply.github.com>
2020-07-19 16:30:18 +0530

    Update detect_faces.py
    script to run face detector and get the output

--- detect_faces.py
+++ detect_faces.py
@@ -11,8 +11,7 @@ Created on Sun Jul 19 14:19:17 2020
 import numpy as np
 import argparse
 import cv2
-import os
-os.chdir("/Users/apple/python_projects/FaceDetection_Ocv/face_detector")
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -28,13 +27,13 @@ args = vars(ap.parse_args())
 
 # load our serialized model from disk
 print("[INFO] loading model...")
-# net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'res10_300x300_ssd_iter_140000.caffemodel')
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+#net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'res10_300x300_ssd_iter_140000.caffemodel')
 
 # load the input image and construct an input blob for the image
 # by resizing to a fixed 300x300 pixels and then normalizing it
-# image = cv2.imread(args["image"])
-image = cv2.imread('/Users/apple/Downloads/gudda_img/im1.jpeg')
+image = cv2.imread(args["image"])
+#image = cv2.imread('/Users/apple/Downloads/gudda_img/im1.jpeg')
 
 (h, w) = image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
@@ -67,32 +66,6 @@ for i in range(0, detections.shape[2]):
 			(0, 0, 255), 2)
 		cv2.putText(image, text, (startX, y),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-# show the output image
-cv2.imshow("Output", image)
-cv2.waitKey(0)
-
-# for i in range(0, detections.shape[2]):
-# 	# extract the confidence (i.e., probability) associated with the
-# 	# prediction
-# 	confidence = detections[0, 0, i, 2]
-# 	# filter out weak detections by ensuring the `confidence` is
-# 	# greater than the minimum confidence
-# 	if confidence > 0.5:
-# 		# compute the (x, y)-coordinates of the bounding box for the
-# 		# object
-# 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-# 		(startX, startY, endX, endY) = box.astype("int")
- 
-# 		# draw the bounding box of the face along with the associated
-# 		# probability
-# 		text = "{:.2f}%".format(confidence * 100)
-# 		y = startY - 10 if startY - 10 > 10 else startY + 10
-# 		cv2.rectangle(image, (startX, startY), (endX, endY),
-# 			(0, 0, 255), 2)
-# 		cv2.putText(image, text, (startX, y),
-# 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
-
 # show the output image
 cv2.imshow("Output", image)
 cv2.waitKey(0)
